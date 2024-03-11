ed7114d8fe7915f79e3fed16036d5d35cc1f84a8
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-03-11 17:27:53 +0530

    Updated login page , titles

--- frontend/src/Registration.jsx
+++ frontend/src/Registration.jsx
@@ -61,7 +61,7 @@ function Registration() {
    return (
       <div className="centered-container">
          <div className="container">
-            <h1 className="text-center">PlantexMate</h1>
+            <h1 className="text-center">Tea Leaf Disease Detection </h1>
             <div className="row justify-content-center">
                <div className="col-md-6">
                   <form onSubmit={handleSubmit}>
